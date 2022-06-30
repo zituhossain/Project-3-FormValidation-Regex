b6547bb405860266d5601c456d2f120518b82994
zituhossain <zituhossain@gmail.com>
2022-06-30 15:07:06 +0600

    changes in style.css, add generate button and some js changes

--- main.js
+++ main.js
@@ -86,3 +86,22 @@ submitElm.addEventListener('click', (e) => {
     }
 
 });
+
+
+// Generate Password
+
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function generate() {
+  var generatePassword = "";
+  var password = document.getElementById("password");
+  var characters =
+    "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*?";
+
+  for (var i = 0; i < 10; i++) {
+    generatePassword += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  password.innerHTML = generatePassword;
+});
